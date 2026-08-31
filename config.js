86d4360366735ef0b21564f9ada7f158d2c029b2
khd7924 <k19713036@gmail.com>
2026-08-31 16:43:57 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // 아래 2개만 입력하면 됩니다.
 // publishable key는 브라우저 사용용이며, 반드시 RLS와 함께 사용하세요.
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://utidqzcmgjcyosmvdyji.supabase.co",
+  SUPABASE_URL: "https://utidqzcmgjcyosmvdjyi.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_wwly2g9g4Olg0sSgMb8krQ__jjnUuU1"
 };
